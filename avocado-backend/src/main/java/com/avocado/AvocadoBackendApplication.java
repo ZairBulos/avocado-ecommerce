d3dc2896b0566568674961bb2e1cf8428159d028
ZairBulos <buloszair@gmail.com>
2023-11-01 21:13:19 -0300

    Add initial backend

---
+++ avocado-backend/src/main/java/com/avocado/AvocadoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.avocado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvocadoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvocadoBackendApplication.class, args);
+	}
+
+}
---
+++ avocado-backend/src/main/java/com/avocado/services/BaseService.java
@@ -0,0 +1,15 @@
+package com.avocado.services;
+
+
+import com.avocado.entities.Base;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseService<E extends Base, ID extends Serializable>  {
+    public List<E> findAll() throws Exception;
+    public E findById(ID id) throws Exception;
+    public E save(E entity) throws Exception;
+    public E update(ID id, E entity) throws Exception;
+    public void delete(ID id) throws Exception;
+}
