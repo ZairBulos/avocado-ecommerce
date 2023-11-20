e40f23a84cac5d1b753557d71141dfd1fcd5c8bc
ZairBulos <buloszair@gmail.com>
2023-11-20 12:03:05 -0300

    Add BaseService

---
+++ avocado-frontend/src/types/Endpoint.d.ts
@@ -0,0 +1,6 @@
+export enum Endpoint {
+  ITEM = "items",
+  ORDER = "orders",
+  USER = "users",
+  AUTH = "auth",
+}
---
+++ avocado-frontend/src/types/Page.d.ts
@@ -0,0 +1,30 @@
+import { Base } from "./Base";
+
+export interface Page<T extends Base> {
+  content: T[];
+  pageable: Pageable;
+  last: boolean;
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  number: number;
+  sort: Sort;
+  first: boolean;
+  numberOfElements: number;
+  empty: boolean;
+}
+
+interface Pageable {
+  pageNumber: number;
+  pageSize: number;
+  sort: Sort;
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+interface Sort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
